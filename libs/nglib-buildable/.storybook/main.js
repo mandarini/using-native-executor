d882d207895684467039b1b3ef772c8dc2b04c54
Katerina Skroumpelou <sk.katherine@gmail.com>
2022-03-31 15:41:08 +0300

    added staticDirs in mainjs

--- libs/nglib-buildable/.storybook/main.js
+++ libs/nglib-buildable/.storybook/main.js
@@ -4,13 +4,13 @@ module.exports = {
   ...rootMain,
 
   core: { ...rootMain.core, builder: 'webpack5' },
-
   stories: [
     ...rootMain.stories,
     '../src/lib/**/*.stories.mdx',
     '../src/lib/**/*.stories.@(js|jsx|ts|tsx)',
   ],
   addons: [...rootMain.addons],
+  staticDirs: ['../src/assets'],
   webpackFinal: async (config, { configType }) => {
     // apply any global webpack configs that might have been specified in .storybook/main.js
     if (rootMain.webpackFinal) {
